8ad99aea41607995a86f18d246ee486201977a54
Angus Grieve-Smith <angus@grieve-smith.com>
2019-09-29 14:33:54 +0000

    itemSelect()

--- languagelab/static/client/mediaFormCard.js
+++ languagelab/static/client/mediaFormCard.js
@@ -1,3 +1,5 @@
+import config from "./config.js";
+
 export default class MediaCard extends React.Component {
     constructor(props) {
         super(props);
@@ -22,6 +24,7 @@ export default class MediaCard extends React.Component {
             {
                 "id": inputId,
                 "className": "form-control",
+                "type": "text",
                 "value": this.props.mediaItem[fieldName],
                 "onChange": this.inputChange
             },
@@ -39,11 +42,82 @@ export default class MediaCard extends React.Component {
         );
     }
 
+    fileInput(fieldName, inputId) {
+        return React.createElement(
+            "input",
+            {
+                "type": "file",
+                "className": "form-control-file",
+                "id": inputId,
+                "onChange": this.inputChange
+            },
+            null
+        );
+    }
+
+    fileInputDiv(fieldName) {
+        const fileLabel = "or upload a file";
+        const inputId = [fieldName, this.props.mediaItem.id].join("_");
+        return React.createElement(
+            "div",
+            {"className": "col-sm"},
+            this.itemLabel(fileLabel, inputId),
+            this.fileInput(fieldName, inputId)
+        );
+    }
+
+    itemOption(optionKey, optionValue) {
+        return React.createElement(
+            "option",
+            {"value": optionKey},
+            optionValue
+        );
+    }
+
+    itemSelect(fieldName, options, inputId) {
+        return React.createElement(
+            "select",
+            {
+                "className": "form-control",
+                "id": inputId,
+                "defaultValue": this.props.mediaItem[fieldName]
+            },
+            ...Object.keys(options).map((optionKey) => {
+                return this.itemOption(
+                    optionKey,
+                    options[optionKey]
+                );
+            })
+        );
+    }
+
+    selectDiv(fieldName) {
+        const inputId = [fieldName, this.props.mediaItem.id].join("_");
+        return React.createElement(
+            "div",
+            {"className": "form-group"},
+            this.itemLabel(fieldName, inputId),
+            this.itemSelect(fieldName, config.formatName)
+        );
+    }
+
     firstRow() {
         return React.createElement(
             "div",
             {"className": "form-row"},
-            this.textInputDiv("name")
+            this.textInputDiv("name"),
+            this.textInputDiv("creator"),
+            this.textInputDiv("rights")
+        );
+    }
+
+    secondRow() {
+        return React.createElement(
+            "div",
+            {"className": "form-row"},
+            this.textInputDiv("mediaUrl"),
+            this.fileInputDiv("mediaFile"),
+            this.selectDiv("format")
         );
     }
 
@@ -51,7 +125,8 @@ export default class MediaCard extends React.Component {
         return React.createElement(
             "div",
             {"className": "card-body"},
-            this.firstRow()
+            this.firstRow(),
+            this.secondRow()
         );
     }
 
