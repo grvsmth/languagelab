6ddbd6770e8f904c2546db86c9eb36bbed7c9550
Angus Grieve-Smith <angus@grieve-smith.com>
2019-11-23 20:16:20 +0000

    doExerciseCard.js

---
+++ languagelab/static/client/doExerciseCard.js
@@ -0,0 +1,226 @@
+import config from "./config.js";
+import commonElements from "./commonElements.js";
+import util from "./util.js";
+
+const timeFormat = "HH:mm:ss.S";
+
+export default class DoExerciseCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            "nowPlaying": this.props.mediaItem.mediaUrl
+        };
+    }
+
+    duration(start, end) {
+        const startMoment = new moment(start, timeFormat);
+        const endMoment = new moment(end, timeFormat);
+        const durationMoment = moment.duration(endMoment.diff(startMoment));
+        return util.formatDuration(durationMoment, 3);
+    }
+
+    itemTitle() {
+        return React.createElement(
+            "h5",
+            {"className": "card-title"},
+            this.props.item.name
+        );
+    }
+
+    bySpan() {
+        if (!this.props.users) {
+            return null;
+        }
+
+        return React.createElement(
+            "span",
+            {"className": "text-dark"},
+            " by ",
+            this.props.users[0].username
+        );
+    }
+
+    itemSubtitle() {
+        const timeRange = util.timeRange(
+            this.props.item.startTime,
+            this.props.item.endTime,
+            timeFormat
+        );
+
+        var mediaName = "";
+        if (this.props.mediaItem) {
+            mediaName = this.props.mediaItem.name + ", ";
+
+        }
+
+        return React.createElement(
+            "h6",
+            {"className": "card-subtitle text-muted"},
+            mediaName,
+            timeRange
+        );
+    }
+
+    descriptionRow() {
+        var languageText = "";
+        if (this.props.languages && this.props.languages.length && this.props.languages[0]) {
+            languageText = this.props.languages[0].name + ", ";
+        }
+
+        var mediaCreator = "";
+        if (this.props.mediaItem) {
+            mediaCreator = this.props.mediaItem.creator;
+        }
+
+        return React.createElement(
+            "div",
+            {},
+            languageText,
+            mediaCreator,
+            ": ",
+            this.props.item.description
+        );
+    }
+
+    checkboxClick(event) {
+        this.props.checkClick(
+            "media",
+            this.props.item.id,
+            event.currentTarget.name,
+            event.currentTarget.checked
+        )
+    }
+
+    editClick(event) {
+        this.props.editItem(this.props.item.id);
+    }
+
+    deleteClick(event) {
+        this.props.deleteClick("media", this.props.item.id);
+    }
+
+    editLink() {
+        return React.createElement(
+            "a",
+            {"className": "text-primary", "onClick": this.editClick},
+            "edit"
+        );
+    }
+
+    deleteLink() {
+        return React.createElement(
+            "a",
+            {"className": "text-danger", "onClick": this.deleteClick},
+            "delete"
+        );
+    }
+
+    linkDiv() {
+        if (this.props.activity === "add") {
+            return null;
+        }
+        return React.createElement(
+            "div",
+            {},
+            commonElements.checkboxDiv(
+                "isAvailable",
+                this.props.item.isAvailable,
+                "available",
+                this.props.item.id,
+                this.checkboxClick
+                ),
+            commonElements.checkboxDiv(
+                "isPublic",
+                this.props.item.isPublic,
+                "public",
+                this.props.item.id,
+                this.checkboxClick
+                ),
+            this.editLink(),
+            " ",
+            this.deleteLink()
+        );
+    }
+
+    textDiv(fieldName, options={}) {
+        return React.createElement(
+            "div",
+            options,
+            this.props.item[fieldName]
+        );
+    }
+
+    queueBody(item) {
+        if (!this.props.item.hasOwnProperty("name")) {
+            return null;
+        }
+
+        return React.createElement(
+            "div",
+            {"className": "card-body"},
+            `Loading exercise ${item.exercise}`
+        );
+
+    }
+
+    player() {
+        return React.createElement(
+            "audio",
+            {
+                "id": "audio1",
+                "src": this.state.nowPlaying,
+                "controls": true,
+                "style": {
+                    "width": "100%"
+                }
+            },
+            null
+        );
+
+    }
+
+    playerDiv() {
+        return React.createElement(
+            "div",
+            {"className": ""},
+            this.player()
+        );
+    }
+
+    controls() {
+        return React.createElement(
+            "div",
+            {},
+            "Controls will go here"
+        );
+    }
+
+    cardBody() {
+        if (!this.props.item.hasOwnProperty("name")) {
+            return this.queueBody(this.props.item);
+        }
+
+        return React.createElement(
+            "div",
+            {"className": "card-body"},
+            this.itemTitle(),
+            this.itemSubtitle(),
+            this.descriptionRow(),
+            this.playerDiv(),
+            this.controls()
+        );
+    }
+
+    cardFooter() {
+    }
+
+    render() {
+        console.log("props", this.props);
+        return React.createElement(
+            "div",
+            {"className": "card bg-light"},
+            this.cardBody()
+        );
+    }
+}
