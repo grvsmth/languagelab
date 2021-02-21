d1bcf5eef7af92efa0f3f079d52c7c218e32d237
Angus Grieve-Smith <angus@grieve-smith.com>
2021-02-21 00:14:01 +0000

    HelpCard

---
+++ languagelab/static/client/cards/helpCard.js
@@ -0,0 +1,44 @@
+export default class HelpCard extends React.Component {
+
+    cardTitle() {
+        return React.createElement(
+            "h3",
+            {
+                "className": "card-title"
+            },
+            this.props.helpItem.title
+        );
+    }
+
+    cardText() {
+        return React.createElement(
+            "p",
+            {
+                "className": "card-text"
+            },
+            this.props.helpItem.description
+        );
+    }
+
+    cardBody() {
+        return React.createElement(
+            "div",
+            {
+                "className": "card-body"
+            },
+            this.cardTitle(),
+            this.cardText()
+        );
+    }
+
+    render() {
+        console.log(this.props);
+        return React.createElement(
+            "div",
+            {
+                "className": "card"
+            },
+            this.cardBody()
+        );
+    }
+}
---
+++ languagelab/static/client/help.js
@@ -0,0 +1,13 @@
+const exports = {
+    "lesson": {
+        "title": "Lesson",
+        "description": `A lesson contains a queue of exercises.  Once
+        you've created a lesson, you can go to the Exercises tab and add your
+        exercises to that lesson.`
+    }
+
+
+
+};
+
+export default exports;
