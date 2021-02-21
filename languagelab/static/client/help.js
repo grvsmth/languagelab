695823ab69cdf8a830e91d77b17b8d5cbf80ec80
Angus Grieve-Smith <angus@grieve-smith.com>
2021-02-21 04:14:26 +0000

    Titles and descriptions for help cards.

--- languagelab/static/client/help.js
+++ languagelab/static/client/help.js
@@ -1,13 +1,28 @@
 const exports = {
-    "lesson": {
-        "title": "Lesson",
-        "description": `A lesson contains a queue of exercises.  Once
-        you've created a lesson, you can go to the Exercises tab and add your
-        exercises to that lesson.`
+    "lessons": {
+        "title": "Lessons",
+        "description": `A lesson contains a queue of exercises and some
+        descriptive metadata.  Once you've created a lesson, you can go to the
+        Exercises tab and add your exercises to that lesson.`
+    },
+    "exercises": {
+        "title": "Exercises",
+        "description": `An exercise is based on a segment of a media item.  Once
+        you've added a media item, you can come here and create exercises based
+        on that media item and add them to a lesson.`
+    },
+    "media": {
+        "title": "Media",
+        "description": `A media item is based on a URL for an externally hosted
+        media file, along with metadata (language, type, description, tags).
+        Once you've added a media item, you can create exercises based on it.`
+    },
+    "languages": {
+        "title": "Languages",
+        "description": `This is just a simple mapping of languages with ISO-639
+        codes.  Once you have added a language, you can specify it in the media
+        metadata.`
     }
-
-
-
 };
 
 export default exports;
